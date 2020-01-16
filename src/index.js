18a8c5ec02c913512707131143ed268cefefad41
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-16 14:18:07 -0700

    Setting up Redux for practice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';;
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import { createStore, applyMiddleware } from "redux";
+// import combineReducers from "./store/index";
+// import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
